d289322b429d6cca958a68b6d11c1a60e28c66cc
devjjinii <devjjinii@naver.com>
2021-10-18 16:05:33 +0900

    globalFilter로 설정

---
+++ apigateway-service/src/main/java/com/yujin/apigatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,48 @@
+package com.yujin.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // pre filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Global Filter baseMessage :{}", config.getBaseMessage());
+
+            if(config.isPostLogger()) {
+                log.info("Global Filter Start: request id -> {}", request.getId());
+            }
+
+            // post filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> { // 비동기방식 단일값 Mono
+                log.info("Global Filter End: response code -> {}", response.getStatusCode());
+            }));
+        };
+    }
+
+    @Data
+    public static class Config {
+        // config 정보
+        private String baseMessage;
+        private boolean preLogger; // isPreLogger
+        private boolean postLogger; // isPostLogger
+
+    }
+}
