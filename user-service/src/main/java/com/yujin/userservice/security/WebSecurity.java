892a914dae61c53ca9510ca9f1e1887f53667b54
devjjinii <devjjinii@naver.com>
2022-02-10 17:47:06 +0900

    User AuthenticationFilter 추가

--- user-service/src/main/java/com/yujin/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/yujin/userservice/security/WebSecurity.java
@@ -1,20 +1,52 @@
 package com.yujin.userservice.security;
 
+import com.yujin.userservice.service.UserService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private UserService userService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    public WebSecurity(Environment env, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.env = env;
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
-
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+                .hasIpAddress("") // 통과시키고 싶은 IP 입력
+                .and()
+                .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
 
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    // select pwd from users where email=?
+    // db.pwd(encrypted) == input.pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       // 로그인 처리 (사용자 데이터 검색)
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
--- user-service/src/main/java/com/yujin/userservice/service/UserService.java
+++ user-service/src/main/java/com/yujin/userservice/service/UserService.java
@@ -2,8 +2,9 @@ package com.yujin.userservice.service;
 
 import com.yujin.userservice.dto.UserDto;
 import com.yujin.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
